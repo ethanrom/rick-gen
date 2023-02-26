79cd50d822a1948901ce822671b61f7b1fab0139
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 00:41:41 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,36 +1,17 @@
 import streamlit as st
-import random
+import requests
 
-# list of possible sentence starters
-starters = ["I love", "I hate", "I'm scared of", "I'm excited about", "I'm interested in"]
+st.title("Pun Generator")
 
-# list of possible sentence endings
-endings = [
-    "the color blue.",
-    "pizza.",
-    "going for walks.",
-    "playing video games.",
-    "learning new things.",
-    "Never gonna give you up.",
-]
+user_input = st.text_input("Enter a word to generate puns:", "funny")
 
-def generate_text(seed_phrase):
-    random.seed(seed_phrase)
-    sentences = []
-    # generate 5 random sentences
-    for i in range(5):
-        sentence = random.choice(starters) + " " + random.choice(endings)
-        sentences.append(sentence)
-    # concatenate the sentences and add the "Never gonna give you up" ending
-    text = " ".join(sentences) + " " + random.choice(endings[-1:])
-    return text
-
-# streamlit app
-st.title("Rick Roll Generator")
-
-# get user input
-seed_phrase = st.text_input("Enter a seed phrase to generate a Rick Roll paragraph:")
-
-if seed_phrase:
-    generated_text = generate_text(seed_phrase)
-    st.write(generated_text)
+if st.button("Generate Puns"):
+    url = "https://api.pungenerator.org/generate?word=" + user_input
+    response = requests.get(url)
+    if response.status_code == 200:
+        puns = response.json()["puns"]
+        st.write("Here are some puns based on your word:")
+        for pun in puns:
+            st.write(pun)
+    else:
+        st.write("Error generating puns. Please try again later.")
