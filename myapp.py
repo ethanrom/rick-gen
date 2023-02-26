00a3f9200688b6ee0af15921b24a67e8b8ee0521
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 00:51:44 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,17 +1,62 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
+import colorgram
 import requests
 
-st.title("Pun Generator")
 
-user_input = st.text_input("Enter a word to generate puns:", "funny")
+def get_colors(image, num_colors):
+    """
+    Returns the dominant colors in the image using the colorgram library.
+    """
+    img = Image.open(image)
+    colors = colorgram.extract(img, num_colors)
+    return [tuple(c.rgb) for c in colors]
 
-if st.button("Generate Puns"):
-    url = "https://api.pungenerator.org/generate?word=" + user_input
-    response = requests.get(url)
-    if response.status_code == 200:
-        puns = response.json()["puns"]
-        st.write("Here are some puns based on your word:")
-        for pun in puns:
-            st.write(pun)
-    else:
-        st.write("Error generating puns. Please try again later.")
+
+def apply_palette(palette, image):
+    """
+    Applies the given color palette to the image.
+    """
+    img = Image.open(image)
+    img = np.array(img)
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            # Find the closest color in the palette to the pixel color.
+            pixel_color = tuple(img[i, j])
+            closest_color = min(palette, key=lambda c: sum((a - b) ** 2 for a, b in zip(c, pixel_color)))
+
+            # Replace the pixel color with the closest color from the palette.
+            img[i, j] = closest_color
+
+    return Image.fromarray(img)
+
+
+def main():
+    st.title("Color Palette App")
+
+    # Upload the images
+    image1 = st.file_uploader("Upload the first image", type=["jpg", "jpeg", "png"])
+    image2 = st.file_uploader("Upload the second image", type=["jpg", "jpeg", "png"])
+
+    if image1 and image2:
+        # Get the color palette of the first image
+        num_colors = st.slider("Number of colors", min_value=2, max_value=20, value=5, step=1)
+        colors = get_colors(image1, num_colors)
+
+        # Show the color palette
+        st.subheader("Color Palette")
+        col1, col2, col3, col4, col5 = st.columns(5)
+        for i, color in enumerate(colors):
+            with col1 if i < 5 else col2 if i < 10 else col3 if i < 15 else col4 if i < 20 else col5:
+                st.color_picker(f"Color {i+1}", color)
+
+        # Apply the color palette to the second image
+        if st.button("Apply Color Palette"):
+            image2 = apply_palette(colors, image2)
+            st.image(image2, caption="Result", use_column_width=True)
+
+
+if __name__ == "__main__":
+    main()
