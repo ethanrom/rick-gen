7f22f513ef664d51b140957e98144754f1ad0209
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 01:42:49 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,14 +3,13 @@ import numpy as np
 from PIL import Image
 import colorgram
 
+# Function to extract colors from an image using Colorgram
 def get_colors(image, num_colors):
-    """
-    Returns the dominant colors in the image using the colorgram library.
-    """
     img = Image.open(image)
     colors = colorgram.extract(img, num_colors)
     return [tuple(c.rgb) for c in colors]
 
+# Function to find the closest color in a list to a target color
 def find_closest_color(colors, target):
     colors = np.array(colors)
     target = np.array(target)
@@ -18,6 +17,7 @@ def find_closest_color(colors, target):
     closest_color_index = np.argmin(distances)
     return colors[closest_color_index]
 
+# Function to apply a color palette to an image
 def apply_palette(colors, image):
     img = Image.open(image).convert("RGBA")
     pixels = np.array(img)
@@ -30,32 +30,46 @@ def apply_palette(colors, image):
     img = Image.fromarray(pixels, mode="RGBA")
     return img
 
+# Main function
 def main():
-    st.title("Color Palette App")
+    # Set page title and favicon
+    st.set_page_config(page_title="Color Palette App", page_icon=":art:")
+
+    # Define page layout
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image("https://i.imgur.com/UcRfRhr.png", width=100)
+    with col2:
+        st.title("Color Palette App")
+        st.write("---")
 
     # Upload the images
-    image1 = st.file_uploader("Upload the first image", type=["jpg", "jpeg", "png"])
-    image2 = st.file_uploader("Upload the second image", type=["jpg", "jpeg", "png"])
+    st.write("### Upload Images")
+    st.write("Upload two images, the first one will be used to extract the color palette.")
+    uploaded_file1 = st.file_uploader("Upload the first image", type=["jpg", "jpeg", "png"])
+    uploaded_file2 = st.file_uploader("Upload the second image", type=["jpg", "jpeg", "png"])
 
-    if image1 and image2:
+    if uploaded_file1 and uploaded_file2:
         # Get the color palette of the first image
+        st.write("### Color Palette Selection")
         num_colors = st.slider("Number of colors", min_value=2, max_value=20, value=5, step=1)
-        colors = get_colors(image1, num_colors)
+        colors = get_colors(uploaded_file1, num_colors)
 
-        # Show the color palette
-        st.subheader("Color Palette")
-        col1, col2, col3, col4, col5 = st.columns(5)
-        for i, color in enumerate(colors):
-            hex_color = '#{:02x}{:02x}{:02x}'.format(*color)
-            with col1 if i < 5 else col2 if i < 10 else col3 if i < 15 else col4 if i < 20 else col5:
+        # Display the color palette
+        st.write("Drag and drop the colors to rearrange the palette.")
+        palette_container = st.container()
+        with palette_container:
+            for i, color in enumerate(colors):
                 hex_color = '#{:02x}{:02x}{:02x}'.format(*color)
-                selected_color = st.color_picker(f"Color {i+1}", hex_color)
+                selected_color = st.color_picker(f"Color {i+1}", hex_color, key=f"color{i}")
                 colors[i] = tuple(int(selected_color.lstrip('#')[k:k+2], 16) for k in (0, 2, 4))
 
         # Apply the color palette to the second image
+        st.write("### Result")
         if st.button("Apply Color Palette"):
-            image2 = apply_palette(colors, image2)
-            st.image(image2, caption="Result", use_column_width=True)
+            result_image = apply_palette(colors, uploaded_file2)
+            st.image(result_image, caption="Result", use_column_width=True)
 
+# Run the app
 if __name__ == "__main__":
     main()
