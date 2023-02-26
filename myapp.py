ed1d753fbb6616c5f49ae11d3ec1f2730fed4b22
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-26 23:59:47 +0530

    Create myapp.py

---
+++ myapp.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import nltk
+import random
+
+# Define the seed text and the song lyrics
+seed_text = "Enter your seed text here"
+song_lyrics = ["Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you", "Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you"]
+
+# Define the Streamlit app
+st.title("Text Generator")
+st.write("Enter a seed phrase below and click the button to generate a paragraph that ends with the lyrics of 'Never Gonna Give You Up'.")
+
+# Get the seed text from the user
+seed_text = st.text_input("Seed phrase", seed_text)
+
+# Generate the text
+if st.button("Generate"):
+    # Tokenize the seed text
+    seed_tokens = nltk.word_tokenize(seed_text)
+    # Generate a random number of sentences (between 1 and 5) to add to the seed text
+    num_sentences = random.randint(1, 5)
+    for i in range(num_sentences):
+        # Use the NLTK library to generate the next word based on the previous words
+        next_word = nltk.ConditionalFreqDist(nltk.bigrams(seed_tokens)).max( seed_tokens[-1] )
+        seed_tokens.append(next_word)
+    # Combine the tokens into a single string and capitalize the first letter
+    paragraph = " ".join(seed_tokens)
+    paragraph = paragraph[0].upper() + paragraph[1:]
+    # Add the song lyrics at the end
+    paragraph += "\n" + "\n".join(song_lyrics)
+    st.write(paragraph)
