cda6b18004ef37a5733521a2d6743c498bd2f64a
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 00:27:39 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,16 +1,33 @@
-import random
 import streamlit as st
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-# Define a list of jokes
-jokes = [    "Why did the tomato turn red? Because it saw the salad dressing!",    "Why did the scarecrow win an award? Because he was outstanding in his field!",    "Why do cows wear bells? Because their horns don't work!",    "Why did the chicken cross the playground? To get to the other slide!",    "Why don't scientists trust atoms? Because they make up everything!",    "What do you call a lazy kangaroo? A pouch potato!",]
+from nltk.tokenize import word_tokenize, sent_tokenize
 
-# Define the Streamlit app
-def main():
-    st.title("Random Joke Generator")
-    st.write("Click the button below to generate a random joke!")
-    if st.button("Generate Joke"):
-        joke = random.choice(jokes)
-        st.write(joke)
+seed_phrase = st.text_input("Enter a seed phrase:", "I love")
+generated_text = ""
 
-if __name__ == "__main__":
-    main()
+if seed_phrase:
+    # Tokenize seed phrase
+    seed_tokens = word_tokenize(seed_phrase)
+
+    # Generate text
+    generated_text = seed_phrase + " "
+    for i in range(5):
+        # Get the last word of the generated text
+        last_word = generated_text.split()[-1]
+        
+        # Generate the next word based on the last word using a bigram model
+        cfd = nltk.ConditionalFreqDist(nltk.bigrams(word_tokenize(generated_text)))
+        if last_word in cfd:
+            next_word = cfd[last_word].max()
+        else:
+            next_word = ""
+
+        generated_text += next_word + " "
+
+    # End the generated text with "Never Gonna Give You Up"
+    generated_text += "Never gonna give you up."
+
+st.write(generated_text)
