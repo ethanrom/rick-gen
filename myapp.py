3a8fcf1c537e43b6127aac2935320a205af2db79
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 00:34:17 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,27 +1,36 @@
 import streamlit as st
-import markovify
+import random
 
-# Define the function to generate text using Markov Chain
-def generate_text(text_input):
-    text_model = markovify.Text(text_input, state_size=2)
-    generated_text = text_model.make_sentence()
-    return generated_text
+# list of possible sentence starters
+starters = ["I love", "I hate", "I'm scared of", "I'm excited about", "I'm interested in"]
 
-# Define the Streamlit app
-def app():
-    st.title("Rick Roll Generator")
-    st.write("Enter a seed phrase to generate text:")
-    
-    # Input for the seed phrase
-    seed_phrase = st.text_input("Seed Phrase:")
-    
-    # Generate text using the seed phrase
-    if st.button("Generate"):
-        if seed_phrase:
-            generated_text = generate_text(seed_phrase)
-            if generated_text:
-                st.write(generated_text.capitalize() + ". Never gonna give you up.")
-            else:
-                st.write("Sorry, could not generate text. Please try a different seed phrase.")
-        else:
-            st.write("Please enter a seed phrase to generate text.")
+# list of possible sentence endings
+endings = [
+    "the color blue.",
+    "pizza.",
+    "going for walks.",
+    "playing video games.",
+    "learning new things.",
+    "Never gonna give you up.",
+]
+
+def generate_text(seed_phrase):
+    random.seed(seed_phrase)
+    sentences = []
+    # generate 5 random sentences
+    for i in range(5):
+        sentence = random.choice(starters) + " " + random.choice(endings)
+        sentences.append(sentence)
+    # concatenate the sentences and add the "Never gonna give you up" ending
+    text = " ".join(sentences) + " " + random.choice(endings[-1:])
+    return text
+
+# streamlit app
+st.title("Rick Roll Generator")
+
+# get user input
+seed_phrase = st.text_input("Enter a seed phrase to generate a Rick Roll paragraph:")
+
+if seed_phrase:
+    generated_text = generate_text(seed_phrase)
+    st.write(generated_text)
